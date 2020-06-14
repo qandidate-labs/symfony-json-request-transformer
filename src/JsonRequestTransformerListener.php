f5d262ada498261d4a716ee21c7df997bb352cba
Jacob Dreesen <jacob@hdreesen.de>
2020-06-14 18:55:48 +0200

    Fix Symfony 5.1 deprecation
    Since symfony/http-foundation 5.1: The "Symfony\Component\HttpFoundation\Response::create()" method is deprecated, use "new Symfony\Component\HttpFoundation\Response()" instead.

--- src/JsonRequestTransformerListener.php
+++ src/JsonRequestTransformerListener.php
@@ -37,7 +37,7 @@ class JsonRequestTransformerListener
         }
 
         if (!$this->transformJsonBody($request)) {
-            $response = Response::create('Unable to parse request.', Response::HTTP_BAD_REQUEST);
+            $response = new Response('Unable to parse request.', Response::HTTP_BAD_REQUEST);
             $event->setResponse($response);
         }
     }
